3be205990a420e604a84a563650286930edd72e3
joosep madar <joosep@greeny.cs.tlu.ee>
2023-10-12 17:37:56 +0300

    6 tund tehtud

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-
 const express = require('express');
+const timeInfo = require('./datetime_fnc');
+const fs = require("fs");
 const app = express();
 
-app.set('view engene': 'ejs');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/', (req,res)=>{
-	//res.send('See töötab');
-	//res.download('index.js'); #(lingi avamisel saab kindla faili alla laadida)#
+app.get('/', (req, res)=>{
+	//res.send('See töötab!');
+	//res.download('index.js');
 	res.render('index');
 });
 
-app.get('/test', (req,res)=>{
-	res.send('Test töötab ka');
-	//res.download('index.js'); #(lingi avamisel saab kindla faili alla laadida)#
+app.get('/timenow', (req, res)=>{
+	const dateNow = timeInfo.dateETformatted();
+	const timeNow = timeInfo.timeETformatted();
+	//res.render('timenow');
+	res.render('timenow', {nowD: dateNow, nowT: timeNow});
 });
+
+app.get('/wisdom', (req, res)=>{
+	let folkWisdom = [];
+	fs.readFile('public/txt/vanasonad.txt', 'utf8', (err, data)=>{
+		if(err){
+			throw err;
+		}
+		else {
+			folkWisdom = data.split(';');
+			res.render('justlist', {h1: 'Vanasõnad', wisdom: folkWisdom});
+		}
+	});
+});
+
 
 app.listen(51020);
